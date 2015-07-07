da72b2e07d0baa21fde337ec4e1509ea7febeeda
Matt Marzillo <mmarzillo@gmail.com>
2015-07-06 20:44:31 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,6 @@
 $("#main").append(["Matt Marzillo"]);
+var awesomeThoughts = "AwesomeThoughts";
+var funThoughts = awesomeThoughts.replace("Awesome","Fun");
+
+console.log(awesomeThoughts);
+console.log(funThoughts);
