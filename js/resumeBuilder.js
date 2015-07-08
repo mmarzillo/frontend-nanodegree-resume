bbe9f910bba540fd034c2c9b71c4366894fb30ef
Matt Marzillo <mmarzillo@gmail.com>
2015-07-07 21:02:01 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,5 @@
 var formattedName = HTMLheaderName.replace("%data%","Matt Marzillo");
 $("#header").append(formattedName);
-
-$("#main").append(["Matt Marzillo"]);
-var awesomeThoughts = " My name is Matt and I am Awesome";
-var funThoughts = awesomeThoughts.replace("Awesome","Fun");
-$('#main').append(funThoughts);
+var formattedRole = HTMLheaderName.replace("%data%","Data Scientist");
+$("#header").prepend(formattedRole);
 
