098dbb9dfb2ea00e553bba168cfd97726be8eeee
Matt Marzillo <mmarzillo@gmail.com>
2015-07-08 19:58:49 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,15 +4,4 @@ var role = "Data Scientist";
 var formattedRole = HTMLheaderRole.replace("%data%",role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-var bio = {
-  "name": "Matt Marzillo",
-  "Role": "Advanced Analytics Consultant",
-  "Contacts": {
-  "Phone": "708-837-1631",
-  "email": "mmarzillo@gmail.com"},
-  "Welcome Message": "Hello!",
-  "Skills": ["Full Stack BI Developer (ETL, Report Development, Architecture)","Advanced Analytics (R, Python and SAS)","Strong Healthcare Experience"
-],
-"Picture": "NA"
-};
-$("#header").append(bio.email);
+
