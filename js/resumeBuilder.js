17946c418f9bc30ea98554d058300954afbfd012
Matt Marzillo <mmarzillo@gmail.com>
2015-07-08 19:57:18 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,14 +5,14 @@ var formattedRole = HTMLheaderRole.replace("%data%",role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 var bio = {
-  "name" : "Matt Marzillo",
-  "Role" : "Advanced Analytics Consultant",
-  "Contacts":{
-  "Phone" : "708-837-1631",
-  "E-mail" : "mmarzillo@gmail.com"},
-  "Picture" : "NA",
-  "Welcome Message" : "Hello!",
-  "Skills" : ["Full Stack BI Developer (ETL, Report Development, Architecture)","Advanced Analytics (R, Python and SAS)","Strong Healthcare Experience"
-]
+  "name": "Matt Marzillo",
+  "Role": "Advanced Analytics Consultant",
+  "Contacts": {
+  "Phone": "708-837-1631",
+  "email": "mmarzillo@gmail.com"},
+  "Welcome Message": "Hello!",
+  "Skills": ["Full Stack BI Developer (ETL, Report Development, Architecture)","Advanced Analytics (R, Python and SAS)","Strong Healthcare Experience"
+],
+"Picture": "NA"
 }
-$("#header").append(bio.E-mail);
+$("#header").append(bio.email);
