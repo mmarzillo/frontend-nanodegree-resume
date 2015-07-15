b4ffda23aeff0e390dd861da12b33ea9b1976dba
Matt Marzillo <mmarzillo@gmail.com>
2015-07-14 21:18:23 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -125,7 +125,10 @@ for (job in work.jobs){
   $("#workExperience").append(HTMLworkStart);
   var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
   var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].position);
-  var formattedEmployerTitle = formattedEmployer + formattedTitle; $(".work-entry:last").append(formattedEmployerTitle);
-  var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].years); $(".work-entry:last").append(formattedDates);
-  var formattedDescription= HTMLworkDescription.replace("%data%",work.jobs[job].description); $(".work-entry:last").append(formattedDescription);
+  var formattedEmployerTitle = formattedEmployer + formattedTitle; 
+  $(".work-entry:last").append(formattedEmployerTitle);
+  var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].years); 
+  $(".work-entry:last").append(formattedDates);
+  var formattedDescription= HTMLworkDescription.replace("%data%",work.jobs[job].description); 
+  $(".work-entry:last").append(formattedDescription);
 }
