499b301680948e48ecdafdee4a8aa0b5b093e233
Matt Marzillo <mmarzillo@gmail.com>
2015-07-16 19:25:25 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,9 @@
-//var name = "Matt Marzillo";
-//var formattedName = HTMLheaderName.replace("%data%",name);
-//var role = "Data Scientist";
-//var formattedRole = HTMLheaderRole.replace("%data%",role);
-//$("#header").prepend(formattedRole);
-//$("#header").prepend(formattedName);
+var name = "Matt Marzillo";
+var formattedName = HTMLheaderName.replace("%data%",name);
+var role = "Data Scientist";
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 var bio = {
   "name": "Matt Marzillo",
   "Role": "Advanced Analytics Consultant",
